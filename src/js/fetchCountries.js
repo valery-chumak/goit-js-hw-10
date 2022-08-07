10c4db0d930309369b0f846c1cd53fe14f231753
valery-chumak <blossssom88@gmail.com>
2022-08-07 16:36:32 +0300

    fix

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,11 @@
 export function fetchCountries(name) {
     const BASE_URL = "https://restcountries.com/v3.1";
     return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error('Error fetching data');
+        })
         .catch(() => console.log('Error'));
 }
